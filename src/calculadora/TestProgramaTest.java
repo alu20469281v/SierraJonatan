91b0a003f2bb03df26b0244afca83dd0cc9b069d
Jonatan Sierra <alu20469281v@ieselcaminas.org>
2019-06-02 21:23:55 +0200

    JUnit

---
+++ src/calculadora/TestProgramaTest.java
@@ -0,0 +1,33 @@
+package calculadora;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestProgramaTest {
+
+@Test
+	void test() {
+		System.out.println("4 5 * 7 2 / - 3 %");
+	}
+	
+@Test
+  void test1() {
+		System.out.println("8 5 * 7 2 / - 3 %");
+	}
+
+@Test
+	void test2() {
+			System.out.println("9 4 5 5 * 7 2 / - 3 %");
+	}	
+	
+@Test
+	void test3() {
+		System.out.println("hola");
+}	
+	
+@Test
+	void test4() {
+		System.out.println("fin");
+}	
+}
